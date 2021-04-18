a84c7aada9709356f9bc1d3972650b5298714119
miquelTC <miqueltc@DESKTOP-G55QVS5.localdomain>
2021-04-18 10:06:11 +0200

    declared fee to level up as public

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -4,7 +4,7 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
-    uint levelUpFee = 0.001 ether;
+    uint public levelUpFee = 0.001 ether;
     
     // Modifier to make sure the zombie has enough level to perform an action
     modifier aboveLevel(uint _level, uint _zombieId) {
