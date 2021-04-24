b5ad1e8957fef8bd7f79b6a6310f72d2d90531b9
miquelTC <miqueltc@DESKTOP-G55QVS5.localdomain>
2021-04-24 12:16:48 +0200

    add a test to fail when someone is using someone else zombie to attack

--- test/ZombieAttack.test.js
+++ test/ZombieAttack.test.js
@@ -8,6 +8,24 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('ZombieAttack', ([deployer, user1]) => {
+contract('ZombieAttack', ([deployer, user1]) => {    
+    let zombieattack;
+    
+    beforeEach(async () => {
+        zombieattack = await ZombieAttack.new();
+        await zombieattack.createRandomZombie('deployerZombie', { from: deployer} );
+        await zombieattack.createRandomZombie('user1Zombie', { from: user1} );
+    })
+    
+    describe('battle between zombies', () => {
+        it('failure when non-owner tries to arrack', async() => {
+            await zombieattack.attack(0, 1, { from: user1 }).should.be.rejectedWith(EVM_REVERT);
+        })   
+
+        // it('victory scenario', async() => {
+        //     await zombieattack.setAttackVictoryProbability(100);
+        //     await zombieattack.attack(0, 1)
+        // })
+    })
     
 })
