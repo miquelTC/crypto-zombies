bf86d0c9afa571bbc68e4f06044c627015cbb885
miquelTC <miqueltc@DESKTOP-G55QVS5.localdomain>
2021-04-25 13:28:09 +0200

    complete the attack test

--- test/ZombieAttack.test.js
+++ test/ZombieAttack.test.js
@@ -13,20 +13,15 @@ contract('ZombieAttack', ([deployer, user1]) => {
 
     beforeEach(async () => {
         zombieattack = await ZombieAttack.new();
-        await zombieattack.setCooldownTime(0);
-        await zombieattack.createRandomZombie('deployerZombie', { from: deployer} );
-        await zombieattack.createRandomZombie('user1Zombie', { from: user1} );
     })
     
-    describe('battle between zombies', () => {
-        it('failure when non-owner tries to set probability', async() => {
-            await zombieattack.setAttackVictoryProbability(100, { from: user1 }).should.be.rejectedWith(EVM_REVERT);
-        }) 
-
-        it('failure when non-owner tries to attack', async() => {
-            await zombieattack.attack(0, 1, { from: user1 }).should.be.rejectedWith(EVM_REVERT);
-        })   
-
+    describe('success', () => {
+        beforeEach(async () => {            
+            await zombieattack.setCooldownTime(0);
+            await zombieattack.createRandomZombie('deployerZombie', { from: deployer} );
+            await zombieattack.createRandomZombie('user1Zombie', { from: user1} );
+        })
+   
         it('victory scenario', async() => {
             await zombieattack.setAttackVictoryProbability(100, { from: deployer });
             await zombieattack.attack(0, 1, { from: deployer });
@@ -49,7 +44,38 @@ contract('ZombieAttack', ([deployer, user1]) => {
             const enemyZombie = await zombieattack.zombies(1);
             enemyZombie.winCount.toString().should.equal('1');
             enemyZombie.lossCount.toString().should.equal('0');
-        })
+        })    
+
+        it('feed and multiply', async () => {
+            await zombieattack.setAttackVictoryProbability(100, { from: deployer });
+            await zombieattack.attack(0, 1, { from: deployer });
+            const newZombie = await zombieattack.zombies(2);
+            newZombie.name.should.equal("NoName");
+            
+        })    
     })
-    
+
+    describe('failure', () => {
+        it('failure when the zombie is not ready due to the cooldown', async() => {
+            await zombieattack.setAttackVictoryProbability(100, { from: deployer });
+            await zombieattack.setCooldownTime(1000);
+            await zombieattack.createRandomZombie('deployerZombie', { from: deployer} );
+            await zombieattack.createRandomZombie('user1Zombie', { from: user1} );
+            await zombieattack.attack(0, 1, { from: deployer }).should.be.rejectedWith(EVM_REVERT);
+        })
+        
+        it('failure when non-owner tries to set probability', async() => {
+            await zombieattack.setCooldownTime(0);
+            await zombieattack.createRandomZombie('deployerZombie', { from: deployer} );
+            await zombieattack.createRandomZombie('user1Zombie', { from: user1} );
+            await zombieattack.setAttackVictoryProbability(100, { from: user1 }).should.be.rejectedWith(EVM_REVERT);
+        }) 
+
+        it('failure when non-owner tries to attack', async() => {
+            await zombieattack.setCooldownTime(0);
+            await zombieattack.createRandomZombie('deployerZombie', { from: deployer} );
+            await zombieattack.createRandomZombie('user1Zombie', { from: user1} );           
+            await zombieattack.attack(0, 1, { from: user1 }).should.be.rejectedWith(EVM_REVERT);
+        }) 
+    })    
 })
